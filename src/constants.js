f1d268a7b296fbd00d5fe3091b199d39449f39bd
Aradhya Mahajan <b23ch1008@iitj.ac.in>
2024-05-20 15:00:16 +0530

    app.js setup, API utilities created

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const DB_NAME = "summer-project"
+export const DB_NAME = "summer-project";
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import 'dotenv/config'
-import connectDB from './db/index.js'
+import "dotenv/config";
+import connectDB from "./db/index.js";
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server running at port: ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log("MongoDB connection failed:", error);
+    });
