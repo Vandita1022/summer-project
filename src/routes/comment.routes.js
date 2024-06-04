329768cd8e5d197453e203cf68b97396cdd084e2
Vandita1022 <147363123+Vandita1022@users.noreply.github.com>
2024-06-05 00:03:40 +0530

    Create comment.routes.js

---
+++ src/routes/comment.routes.js
@@ -0,0 +1,28 @@
+//project.routes need to be updated
+//import commentRouter from "./comment.routes.js";
+////Mount comment routes
+//router.use("/:projectId/comment", verifyToken, commentRouter)
+
+import { Router } from "express";
+import commentControllers from "../controllers/comment.controller.js";
+import { asyncHandler } from "../utils/asynchandler.js";
+import { verifyToken } from "../middlewares/auth.middleware.js";
+
+const router = Router({ mergeParams: true });
+
+// Route to get all comments for a project
+router.get("/", verifyToken, asyncHandler(commentControllers.getAllComments));
+
+// Route to create a new comment
+router.post("/", verifyToken, asyncHandler(commentControllers.createComment));
+
+// Route to get a comment by ID
+router.get("/:commentId", verifyToken, asyncHandler(commentControllers.getCommentById));
+
+// Route to update a comment by ID
+router.put("/:commentId", verifyToken, asyncHandler(commentControllers.updateComment));
+
+// Route to delete a comment by ID
+router.delete("/:commentId", verifyToken, asyncHandler(commentControllers.deleteComment));
+
+export default router;
